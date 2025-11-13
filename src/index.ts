cb91f8b5f311885ef9b572ebe0ce18dd4f1d1bea
Sergio Correia <sergiosdcorreia@gmail.com>
2025-11-13 21:41:44 +0000

    Adding CORS for Frontend

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Routes
